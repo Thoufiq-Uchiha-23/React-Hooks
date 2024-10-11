1ec4beedc6d51b2cb2b329c6b9db66fa8a229dee
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-10-12 00:10:42 +0530

    useState concept completed

--- src/components/StateHook.jsx
+++ src/components/StateHook.jsx
@@ -1,27 +1,44 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const StateHook = () => {
-//   const [count, setCount] = useState(4)
-const [state, setState] = useState({count: 4, theme: 'blue'})
-const count = state.count
-const theme = state.theme  
+  const [count, setCount] = useState(4);
+  const [theme, setTheme] = useState("blue");
+  // const [state, setState] = useState({count: 4, theme: 'blue'})
+  // const count = state.count
+  // const theme = state.theme
+
+  //   function decrementCount() {
+  //     setState(prevState => {
+  //         return {...prevState, count: prevState.count - 1}
+  //     })
+  //   }
+
+  //   function incrementCount() {
+  //     setState(prevState => {
+  //         return {...prevState, count: prevState.count + 1}
+  //     })
+  //   }
 
   function decrementCount() {
-    setState(prevCount => prevCount - 1)
-  }
-  
-  function incrementCount() {
-    setState(prevCount => prevCount + 1)
+    setCount((prevCount) => prevCount - 1);
   }
 
+  function incrementCount() {
+    setCount((prevCount) => prevCount + 1);
+    setTheme("red");
+  }
   return (
     <div>
-        <button className="w-7 bg-rose-900 rounded-md" onClick={decrementCount}>-</button>
-        <span className="text-xl font-bold">{count}</span>
-        <span className="text-xl font-bold">{theme}</span>
-        <button className="w-7 bg-rose-900 rounded-md" onClick={incrementCount}>+</button>
+      <button className="w-7 bg-rose-900 rounded-md" onClick={decrementCount}>
+        -
+      </button>
+      <span className="text-xl font-bold">{count}</span>
+      <span className="text-xl font-bold">{theme}</span>
+      <button className="w-7 bg-rose-900 rounded-md" onClick={incrementCount}>
+        +
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default StateHook
+export default StateHook;
