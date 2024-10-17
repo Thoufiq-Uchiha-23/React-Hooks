1705b1a3f2c68ad46d0a884f6836ab2ec3556b73
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-10-17 23:58:28 +0530

    reducer hook

--- src/components/ReducerHook.jsx
+++ src/components/ReducerHook.jsx
@@ -1,4 +1,5 @@
 import { useReducer, useState } from "react";
+import Todo from "./Todo";
 
 // const ACTIONS = {
 //     INCREMENT: 'increment',
@@ -38,23 +39,50 @@ import { useReducer, useState } from "react";
 
 // Making Todos using useReducer hook
 
-const ACTIONS = {};
+const ACTIONS = {
+  ADD_TODO: "add todo",
+  TOGGLE_TODO: "toggle todo",
+};
 
-function reducer(state, action) {}
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)];
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete };
+        }
+        return todo;
+      });
+  }
+}
 
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false };
+}
 const ReducerHook = () => {
   const [todos, dispatch] = useReducer(reducer, []);
   const [name, setName] = useState("");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+    setName("");
+  }
+
   return (
     <div>
-      <form onSubmit={handleSubmit}>+
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
       </form>
+      {todos.map((todo) => {
+        <Todo key={todo.id} todo={todo} />;
+      })}
     </div>
   );
 };
