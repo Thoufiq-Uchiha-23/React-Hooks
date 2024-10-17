9e201d1a56b2e3ec6a5ad8af1a399178899b2751
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-10-17 23:58:37 +0530

    todo

---
+++ src/components/Todo.jsx
@@ -0,0 +1,11 @@
+const Todo = ({todo}) => {
+  return (
+    <div>
+        <span style={{color: todo.complete ? "#AAA" : "#000"}}>{todo.name}</span>
+        <button>Toggle</button>
+        <button>Delete</button>
+    </div>
+  )
+}
+
+export default Todo
