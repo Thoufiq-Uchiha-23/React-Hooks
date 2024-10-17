50a86ece5fc9e366b984174c4b4eeeb36e86531e
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-10-18 02:38:39 +0530

    Todo Component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,24 @@
-const Todo = ({todo}) => {
+import PropTypes from "prop-types"
+import { ACTIONS } from "./ReducerHook";
+
+const Todo = ({todo, dispatch}) => {
   return (
-    <div>
+    <div key={todo.id}  >
         <span style={{color: todo.complete ? "#AAA" : "#000"}}>{todo.name}</span>
-        <button>Toggle</button>
-        <button>Delete</button>
+        <button onClick={() => dispatch({type: ACTIONS.TOGGLE_TODO, payload: {id: todo.id}})}>Toggle</button>
+        <button onClick={() => dispatch({type: ACTIONS.DELETE_TODO, payload: {id: todo.id}})}>Delete</button>
     </div>
   )
 }
+
+Todo.propTypes = {
+  todo: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    complete: PropTypes.bool.isRequired,
+  }).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
 
 export default Todo
