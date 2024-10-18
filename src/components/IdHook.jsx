c42c886141fefc45eb3a032ba2513d3da42a30a4
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-10-18 22:35:16 +0530

    useId

---
+++ src/components/IdHook.jsx
@@ -0,0 +1,22 @@
+import EmailForm from "./EmailForm";
+
+const IdHook = () => {
+  return (
+    <div>
+      <EmailForm />
+      <article style={{ marginBlock: "1rem" }}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque libero
+        facilis asperiores modi, voluptatem soluta enim officia, earum at
+        obcaecati quia non ullam et tenetur eos dolore explicabo neque
+        dignissimos reiciendis, odit ab voluptates nemo! Magni at iste
+        doloremque quam neque quae eveniet veritatis, veniam magnam illum odit
+        enim quas. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Adipisci in debitis eius enim ullam natus animi provident voluptas ipsam
+        nihil!
+      </article>
+      <EmailForm />
+    </div>
+  );
+};
+
+export default IdHook;
